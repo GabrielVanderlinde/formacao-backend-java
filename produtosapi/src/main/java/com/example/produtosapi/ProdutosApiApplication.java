0ae08edda162d4ecd42916d2b7840a4db48e1e6b
Gabriel Vanderlinde <gabrielvanderlindezz@gmail.com>
2026-01-22 13:57:15 -0300

    refactor: remove endpoint hello-world

--- produtosapi/src/main/java/com/example/produtosapi/ProdutosApiApplication.java
+++ produtosapi/src/main/java/com/example/produtosapi/ProdutosApiApplication.java
@@ -2,17 +2,9 @@ package com.example.produtosapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ProdutosApiApplication {
-
-    @GetMapping("/hello-world")
-    public String HelloWorld() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProdutosApiApplication.class, args);
