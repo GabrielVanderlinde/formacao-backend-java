998afea1f2a275d7227abc4d2cd33fd38bdd1cfb
GabrielVanderlinde <gabrielvanderlindezz@gmail.com>
2026-02-17 10:38:28 -0300

    feat: cria mapeamento JPA para entidade Produto

--- produtosapi/src/main/java/com/example/produtosapi/model/Produto.java
+++ produtosapi/src/main/java/com/example/produtosapi/model/Produto.java
@@ -1,27 +1,32 @@
 package com.example.produtosapi.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
+@Table(name = "produto")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @ToString
+
 public class Produto {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco")
     private double preco;
 
 }
